9f0c6866598b95ac4418518a65c4c61b61ee39d4
hwangseonu <hwangseonu12@naver.com>
2020-05-17 01:53:40 +0900

    Auto stash before merge of "master" and "origin/master"

--- src/containers/Login.tsx
+++ src/containers/Login.tsx
@@ -1,36 +1,10 @@
 import React, {useState} from 'react';
 import { Redirect } from 'react-router-dom';
-import styled from 'styled-components';
-import Color from '../assets/javascripts/color';
-import kiwi from '../assets/images/kiwi.svg';
-import LoginForm from '../components/Login/LoginForm';
 import { IpcRendererEvent } from 'electron'
-
 import { getIpcRenderer } from '../functions/electron';
 
-const Wrapper = styled.div`
-width: 100%;
-height: 100vh;
-background: linear-gradient(128.19deg, ${Color.THEME1} 30.01%, ${Color.THEME2} 47.2%);
-`;
-
-const Kiwi = styled.img`
-position: fixed;
-width: 100%;
-height: 100%;
-right: -30%;
-bottom: -50%;
-user-select: none;
-`;
-
-const VersionText = styled.span`
-position: fixed;
-left: 13px;
-bottom: 10px;
-font-size: 8px;
-z-index: 2;
-user-select: none;
-`;
+import LoginBackground from '../components/Login/LoginBackground';
+import LoginForm from '../components/Login/LoginForm';
 
 const resultText: {[key: string]: string} = {
   success: '로그인 성공',
@@ -47,6 +21,7 @@ interface LoginResponse {
 
 const Login = () => {
   const [redirect, setRedirect] = useState('');
+  
   const onSubmit = (email: string, password: string) => {
     const ipcRenderer = getIpcRenderer();
 
@@ -66,11 +41,9 @@ const Login = () => {
   };
 
   return redirect ? <Redirect to={redirect}/> : (
-    <Wrapper>
+    <LoginBackground>
       <LoginForm onSubmit={onSubmit}/>
-      <Kiwi src={kiwi} alt={'kiwi'}/>
-      <VersionText><b>Kiwitalk</b> ver 0.0.1</VersionText>
-    </Wrapper>
+    </LoginBackground>
   )
 };
 
