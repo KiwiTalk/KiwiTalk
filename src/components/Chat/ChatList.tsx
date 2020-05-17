1d2cf4c2d857a5aba81ba652aaff32d880bba758
hwangseonu <hwangseonu12@naver.com>
2020-05-18 00:54:14 +0900

    use getChannelInfo

--- src/components/Chat/ChatList.tsx
+++ src/components/Chat/ChatList.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {EventHandler, MouseEventHandler, useEffect, useState} from 'react';
 import styled from 'styled-components';
 import ChatListItem from './ChatListItem';
 import ProfileDefault from '../../assets/images/profile_default.svg'
@@ -31,39 +31,44 @@ function extractRoomImage (channelInfo: ChatChannel['channelInfo']) {
       imageUrl = content.imageUrl
     }
   })
-
+  console.log(channelInfo);
   return imageUrl
 }
 
+const AsyncComponent: React.FC<{channel: ChatChannel, selected: boolean, onClick: MouseEventHandler}> = ({channel, selected, onClick}) => {
+  const [comp, setComp] = useState<JSX.Element>();
+
+  useEffect(() => {
+    channel.getChannelInfo().then(ch => {
+      console.log(ch);
+
+      setComp(<ChatListItem
+        key={ channel.Id.low }
+        lastChat={ channel.LastChat ? channel.LastChat.Text : '' }
+        profileImageSrc={ extractRoomImage(ch) }
+        username={ ch.Name }
+        selected={ selected }
+        onClick={onClick}/>)
+    });
+  }, [])
+
+  return (
+    <React.Fragment>
+      { comp }
+    </React.Fragment>
+  );
+};
+
 const ChatList: React.FC<ChatListProps> = ({ channelList, onChange }) => {
   const [selectedIndex, setSelectedIndex] = useState(0);
+
   return (
       <Wrapper>
-        {
-          channelList.map((channel, index) => <ChatListItem
-              key={
-                channel.Id.low
-              }
-              lastChat={
-                channel.LastChat ? channel.LastChat.Text : ''
-              }
-              profileImageSrc={
-                extractRoomImage(channel["channelInfo"])
-              }
-              username={
-                channel["channelInfo"].Name
-              }
-              selected={
-                selectedIndex === index
-              }
-              onClick={
-                () => {
-                  setSelectedIndex(index);
-                  onChange && onChange(index);
-                }
-              }
-          />)
-        }
+        { channelList.map((channel, index) =>
+          <AsyncComponent channel={channel} selected={selectedIndex === index} onClick={() => {
+            setSelectedIndex(index);
+            onChange && onChange(index);
+          }}/>) }
       </Wrapper>
   );
 };
