689b6b2cd26c4cf61d29def80b1847005d1c7bb9
JellyBrick <shlee1503@naver.com>
2020-05-21 12:25:52 +0900

    Fix #22

--- src/components/UiComponent/Bubble.tsx
+++ src/components/UiComponent/Bubble.tsx
@@ -83,7 +83,7 @@ const convertTime = (time: Date, use24format = true) => {
   const hour = time.getHours();
   const minute = time.getMinutes();
 
-  let hourStr = use24format ? hour.toString() : hour < 13 ? `오전 ${hour === 0 ? 12 : hour}` : `오후 ${hour - 12}`;
+  let hourStr = use24format ? hour.toString() : hour < 12 ? `오전 ${hour === 0 ? 12 : hour}` : `오후 ${hour === 12 ? hour : hour - 12}`;
 
   return `${hourStr}:${minute < 10 ? `0${minute}` : minute}`
 }
