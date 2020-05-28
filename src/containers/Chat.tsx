f1eb8afa5419db76f9d1c191b9d539036a75a87e
jellybrick <shlee1503@naver.com>
2020-05-28 11:12:00 +0900

    getChatListBetween으로 변경

--- src/containers/Chat.tsx
+++ src/containers/Chat.tsx
@@ -61,7 +61,7 @@ const Chat = () => {
             if (firstMessage.ChatList.length) {
                 let start_id = (firstMessage.ChatList.shift() as ChatlogStruct).prevLogId, chatLog: ChatObject[] = [];
                 do {
-                    chatLog = await talkClient.ChatManager.getChatListFrom(channel.Id, start_id, 1, lastTokenId);
+                    chatLog = await talkClient.ChatManager.getChatListBetween(channel.Id, start_id, 1, lastTokenId);
                     update.push(...chatLog);
 
                     if (chatLog.length > 0) {
