881e89bd6f305d276eb6fb54e4a10384db1a8b4e
storycraft <storycraft@pancake.sh>
2022-09-02 23:50:18 +0900

    Add model constant

--- crates/kiwi-talk-client/src/constants.rs
+++ crates/kiwi-talk-client/src/constants.rs
@@ -1,6 +1,7 @@
 pub const TALK_VERSION: &str = "3.4.2";
 pub const TALK_OS: &str = "win32";
 pub const TALK_MCCMNC: &str = "999";
+pub const TALK_MODEL: &str = "";
 
 pub const BOOKING_SERVER_DOMAIN: &str = "booking-loco.kakao.com";
 pub const BOOKING_SERVER: &str = "booking-loco.kakao.com:443";
