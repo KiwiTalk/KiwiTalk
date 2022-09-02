b607bcfb7ba357d17d552fa8ba61c6616805d2fc
storycraft <storycraft@pancake.sh>
2022-09-02 23:43:00 +0900

    Add server addr, os, mccmnc constants

--- crates/kiwi-talk-client/src/constants.rs
+++ crates/kiwi-talk-client/src/constants.rs
@@ -1,1 +1,8 @@
 pub const TALK_VERSION: &str = "3.4.2";
+pub const TALK_OS: &str = "win32";
+pub const TALK_MCCMNC: &str = "999";
+
+pub const BOOKING_SERVER_DOMAIN: &str = "booking-loco.kakao.com";
+pub const BOOKING_SERVER: &str = "booking-loco.kakao.com:443";
+
+pub const CHECKIN_SERVER: &str = "ticket-loco.kakao.com";
