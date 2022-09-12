3d98c232c870edfca59902a4fed7ce578cae5638
storycraft <storycraft@pancake.sh>
2022-09-12 15:00:38 +0900

    Fix LoginListRes, LChatListRes

--- crates/talk-loco-command/src/response/chat/l_chat_list.rs
+++ crates/talk-loco-command/src/response/chat/l_chat_list.rs
@@ -4,6 +4,36 @@ use serde::{Deserialize, Serialize};
 /// Request every chatroom list
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct LChatListRes {
+    /// false if there is more channels to be requested with LCHATLIST
+    pub eof: bool,
+
+    /// Latest mcm(?) revision
+    #[serde(rename = "mcmRevision")]
+    pub mcm_revision: i64,
+
+    /// Latest chatroom id
+    #[serde(rename = "lastChatId")]
+    pub last_chat_id: Option<i64>,
+
+    /// Latest token(Unknown) id
+    #[serde(rename = "lastTokenId")]
+    pub last_token_id: Option<i64>,
+
+    /// Latest token(Unknown)(?)
+    #[serde(rename = "ltk")]
+    pub last_token: Option<i64>,
+
+    /// Latest block token(Unknown)(?)
+    #[serde(rename = "lbk")]
+    pub last_block_token: i64,
+
+    // Unknown, Unknown item type
+    //pub kc: Vec<()>
+
+    /// Deleted chatroom ids(?)
+    #[serde(rename = "delChatIds")]
+    pub deleted_chat_ids: Vec<i64>,
+
     #[serde(rename = "chatDatas")]
     pub chat_datas: Vec<ChannelListData>,
 }
--- crates/talk-loco-command/src/response/chat/login_list.rs
+++ crates/talk-loco-command/src/response/chat/login_list.rs
@@ -15,46 +15,29 @@ pub struct LoginListRes {
     #[serde(flatten)]
     pub chat_list: LChatListRes,
 
-    /// Deleted chatroom ids(?)
-    #[serde(rename = "delChatIds")]
-    pub deleted_chat_ids: Vec<i64>,
-
-    /// Unknown
-    pub eof: bool,
-
-    /// Latest chatroom id
-    #[serde(rename = "lastChatId")]
-    pub last_chat_id: Option<i64>,
-
-    /// Latest token(Unknown) id
-    #[serde(rename = "lastTokenId")]
-    pub last_token_id: i64,
-
     /// Oldest chat id (?)
     #[serde(rename = "minLogId")]
-    pub min_log_id: i64,
+    pub min_log_id: Option<i64>,
 
-    /// Latest token(Unknown)(?)
-    #[serde(rename = "ltk")]
-    pub last_token: i64,
+    /// Unknown (Mobile only)
+    pub revision: Option<i32>,
 
-    /// Latest block token(Unknown)(?)
-    #[serde(rename = "lbk")]
-    pub last_block_token: i64,
-
-    /// Latest mcm(?) revision
-    #[serde(rename = "mcmRevision")]
-    pub mcm_revision: i64,
-
-    /// Unknown
-    pub revision: i32,
-
-    /// Revision(?) Info (Json)
+    /// Revision(?) Info (Json) (Mobile only)
     #[serde(rename = "revisionInfo")]
-    pub revision_info: String,
+    pub revision_info: Option<String>,
 
     /// Unknown
     pub sb: i32,
-    // Unknown, Unknown item type
-    //pub kc: Vec<()>
+
+    /// 6 bytes binary
+    #[serde(with = "serde_bytes")]
+    pub rp: Vec<u8>,
+    
+    /// Unknown
+    #[serde(rename = "pkUpdate")]
+    pub pk_update: Option<bool>,
+
+    /// Unknown
+    #[serde(rename = "pkToken")]
+    pub pk_token: Option<i64>,
 }
