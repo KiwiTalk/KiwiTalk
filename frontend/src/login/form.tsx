c904e5cbfee52f2da79d20697610169433d58ecc
storycraft <storycraft@pancake.sh>
2022-09-25 23:19:29 +0900

    Add login form

---
+++ frontend/src/login/form.tsx
@@ -0,0 +1,88 @@
+import styled from 'styled-components';
+import { Button } from '../components/button';
+import { CheckBox } from '../components/check-box';
+import { InputForm } from '../components/input-form';
+
+const LoginInput = styled(InputForm)`
+  display: block;
+  margin-bottom: 12px;
+`;
+
+const LoginButton = styled(Button)`
+  display: block;
+  margin: 12px 0px 0px 0px;
+  width: 100%;
+`;
+
+const LoginCheckbox = styled(CheckBox)`
+  display: block;
+  margin-top: 10px;
+`;
+
+export type LoginFormInput = {
+  email: string,
+  password: string,
+  saveId: boolean,
+  autoLogin: boolean
+}
+
+export type LoginFormProp = {
+  input?: Partial<LoginFormInput>,
+  onSubmit?: (input: LoginFormInput) => void,
+
+  className?: string
+}
+
+export const LoginForm: React.FC<LoginFormProp> = ({
+  input,
+  onSubmit,
+
+  className,
+}) => {
+  const currentInput: LoginFormInput = {
+    email: '',
+    password: '',
+    saveId: false,
+    autoLogin: false,
+    ...input,
+  };
+
+  function clickHandler() {
+    if (onSubmit) {
+      onSubmit(currentInput);
+    }
+  }
+
+  // TODO:: Update placeholder text
+  return <div className={className}>
+    <LoginInput
+      icon='account_circle'
+      input={{
+        type: 'email',
+        placeholder: '카카오계정(이메일 또는 전화번호)',
+        defaultValue: currentInput.email,
+      }}
+      onChange={(text) => currentInput.email = text}
+    />
+    <LoginInput
+      icon='vpn_key'
+      input={{
+        type: 'password',
+        placeholder: '비밀번호',
+        defaultValue: currentInput.password,
+      }}
+      onChange={(text) => currentInput.password = text}
+    />
+    <LoginButton onClick={() => clickHandler()}>로그인</LoginButton>
+    <LoginCheckbox
+      id='saveId'
+      checked={currentInput.saveId}
+      onChange={(status) => currentInput.saveId = status.checked || false}
+    >아이디 저장</LoginCheckbox>
+    <LoginCheckbox
+      id='autoLogin'
+      checked={currentInput.autoLogin}
+      onChange={(status) => currentInput.autoLogin = status.checked || false}
+    >실행 시 자동 로그인</LoginCheckbox>
+  </div>;
+};
