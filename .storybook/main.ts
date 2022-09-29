1bdde7033ea8c68b05fbfadb03e62f63708dee4b
storycraft <storycraft@pancake.sh>
2022-09-30 02:35:51 +0900

    Remove double quotes

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,21 +3,21 @@ import { mergeConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
 
 const config: StorybookViteConfig = {
-  "stories": [
+  stories: [
     "../frontend/src/**/*.stories.mdx",
     "../frontend/src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions"
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-vite"
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-vite"
   },
-  "features": {
-    "storyStoreV7": true
+  features: {
+    storyStoreV7: true
   },
   async viteFinal(config) {
     return mergeConfig(config, {
