923ecd7a4ef89279cfb5899561d0806d28c4ae67
storycraft <storycraft@pancake.sh>
2022-10-10 15:42:14 +0900

    Add onLogin

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -4,8 +4,11 @@ import { useAsync } from '../hooks/async';
 
 import { AppLogin } from './login';
 import i18next from 'i18next';
-import { getGlobalConfiguration } from '../backend/app';
+import { getGlobalConfiguration, setCredential, initializeClient } from '../backend/app';
 import { getDeviceLocale } from '../backend/system';
+import { useEffect } from 'react';
+import { LoginAccessData } from '../backend/auth';
+import { createClientSession } from './session';
 
 export const App = () => {
   const data = useAsync(async () => {
@@ -18,17 +21,35 @@ export const App = () => {
     };
   });
 
-  if (data.status === 'pending' || data.status == 'error') {
-    return;
-  }
+  useEffect(() => {
+    if (data.status === 'pending' || data.status == 'error') {
+      return;
+    }
 
-  if (data.value.configuration.locale.type == 'Auto') {
-    i18next.changeLanguage(data.value.deviceLocale);
-  } else {
-    i18next.changeLanguage(data.value.configuration.locale.value);
+    if (data.value.configuration.locale.type == 'Auto') {
+      i18next.changeLanguage(data.value.deviceLocale);
+    } else {
+      i18next.changeLanguage(data.value.configuration.locale.value);
+    }
+  }, [data.status]);
+
+  function onLogin(data: LoginAccessData) {
+    (async () => {
+      await setCredential({
+        access_token: data.access_token,
+        refresh_token: data.refresh_token,
+        userId: data.userId,
+      });
+
+      await initializeClient({ status: 'Unlocked' });
+
+      for await (const event of createClientSession()) {
+        // TODO::
+      }
+    })().then();
   }
 
   return <>
-    <AppLogin />
+    <AppLogin onLogin={onLogin} />
   </>;
 };
