5bee27bf2fbcf7c139747d8906f4329545417319
storycraft <storycraft@pancake.sh>
2022-12-20 01:41:55 +0900

    feat: add todo placeholder component

---
+++ frontend/src/components/todo-placeholder/index.tsx
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+import { ReactComponent as InfoIconSvg } from './icons/info_black_24dp.svg';
+
+const Container = styled.div`
+  padding: 1rem;
+`;
+
+const Inner = styled.div`
+  padding: 1rem;
+
+  border: 1px solid #DFDEE0;
+  border-radius: 8px;
+
+  color: gray;
+  
+  text-align: center;
+
+  user-select: none;
+`;
+
+const InfoIcon = styled(InfoIconSvg)`
+  width: 4rem;
+  height: 4rem;
+`;
+
+export type TodoPlaceholderProp = {
+  part: string
+}
+
+export const TodoPlaceholder = ({
+  part,
+}: TodoPlaceholderProp) => {
+  return <Container>
+    <Inner>
+      <InfoIcon />
+      <p>
+        {part} is not implemented yet...
+      </p>
+    </Inner>
+  </Container>;
+};
