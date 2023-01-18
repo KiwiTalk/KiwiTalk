3b9b76fed83f21f00c32b66ae5764f9c995348a7
storycraft <storycraft@pancake.sh>
2023-01-18 14:20:54 +0900

    fix(talk-client): replace deprecated base64 methods

--- crates/kiwi-talk-app/src/system.rs
+++ crates/kiwi-talk-app/src/system.rs
@@ -1,5 +1,6 @@
 use std::path::PathBuf;
 
+use base64::{engine::general_purpose::STANDARD, Engine};
 use platform_dirs::AppDirs;
 use rand::Rng;
 use tauri::{
@@ -68,7 +69,7 @@ pub struct DeviceUuid(String);
 
 impl DeviceUuid {
     pub fn new(data: &[u8; 64]) -> Self {
-        DeviceUuid(base64::encode(&data))
+        DeviceUuid(STANDARD.encode(&data))
     }
 
     #[inline]
@@ -77,7 +78,7 @@ impl DeviceUuid {
     }
 
     pub fn decode(&self) -> Vec<u8> {
-        base64::decode(&self.0).unwrap()
+        STANDARD.decode(&self.0).unwrap()
     }
 }
 
