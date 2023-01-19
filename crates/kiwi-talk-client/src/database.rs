0c3e586d7bbd90c7c0b45cb35ad3ead9c15c9efd
storycraft <storycraft@pancake.sh>
2023-01-20 03:55:32 +0900

    feat(talk-client): add ConnectionManagerError feat(talk-client): initialize database on first connection

--- crates/kiwi-talk-client/src/database.rs
+++ crates/kiwi-talk-client/src/database.rs
@@ -1,14 +1,19 @@
-use std::path::Path;
+use std::{
+    path::Path,
+    sync::atomic::{AtomicBool, Ordering},
+};
 
-use kiwi_talk_db::{rusqlite, KiwiTalkConnection};
+use kiwi_talk_db::{rusqlite, KiwiTalkConnection, rusqlite_migration};
 use r2d2::{ManageConnection, Pool};
 use r2d2_sqlite::SqliteConnectionManager;
+use thiserror::Error;
 
 pub type KiwiTalkDatabasePool = Pool<KiwiTalkDatabaseManager>;
 
 #[derive(Debug)]
 pub struct KiwiTalkDatabaseManager {
     rusqlite: SqliteConnectionManager,
+    initialized: AtomicBool,
 }
 
 impl KiwiTalkDatabaseManager {
@@ -16,6 +21,7 @@ impl KiwiTalkDatabaseManager {
     pub fn file(path: impl AsRef<Path>) -> Self {
         Self {
             rusqlite: SqliteConnectionManager::file(path),
+            initialized: AtomicBool::new(false),
         }
     }
 
@@ -23,6 +29,7 @@ impl KiwiTalkDatabaseManager {
     pub fn memory() -> Self {
         Self {
             rusqlite: SqliteConnectionManager::memory(),
+            initialized: AtomicBool::new(false),
         }
     }
 }
@@ -30,10 +37,20 @@ impl KiwiTalkDatabaseManager {
 impl ManageConnection for KiwiTalkDatabaseManager {
     type Connection = KiwiTalkConnection;
 
-    type Error = rusqlite::Error;
+    type Error = ConnectionManagerError;
 
     fn connect(&self) -> Result<Self::Connection, Self::Error> {
-        Ok(KiwiTalkConnection::new(self.rusqlite.connect()?))
+        let mut connection = KiwiTalkConnection::new(self.rusqlite.connect()?);
+
+        if self
+            .initialized
+            .compare_exchange(false, true, Ordering::SeqCst, Ordering::Acquire)
+            .is_ok()
+        {
+            connection.migrate_to_latest()?;
+        }
+
+        Ok(connection)
     }
 
     fn is_valid(&self, conn: &mut Self::Connection) -> Result<(), Self::Error> {
@@ -43,4 +60,13 @@ impl ManageConnection for KiwiTalkDatabaseManager {
     fn has_broken(&self, _: &mut Self::Connection) -> bool {
         false
     }
+}
+
+#[derive(Debug, Error)]
+pub enum ConnectionManagerError {
+    #[error(transparent)]
+    Rusqlite(#[from] rusqlite::Error),
+
+    #[error(transparent)]
+    Initialization(#[from] rusqlite_migration::Error),
 }
