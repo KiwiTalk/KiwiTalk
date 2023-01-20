9c6328dce1609c7bb93795045176f367f6ffb154
storycraft <storycraft@pancake.sh>
2023-01-21 00:02:54 +0900

    feat(talk-client): add channel_user_model_from_user_variant

--- crates/kiwi-talk-client/src/database/conversion.rs
+++ crates/kiwi-talk-client/src/database/conversion.rs
@@ -1,9 +1,9 @@
 use kiwi_talk_db::{
-    channel::model::{ChannelId, ChannelModel},
+    channel::model::{ChannelId, ChannelModel, ChannelUserId, ChannelUserModel},
     chat::model::{ChatModel, LogId},
     model::FullModel,
 };
-use talk_loco_command::structs::{channel_info::ChannelListData, chat::Chatlog};
+use talk_loco_command::structs::{channel_info::ChannelListData, chat::Chatlog, user::UserVariant};
 
 // TODO:: use trait
 pub fn chat_model_from_chatlog(chatlog: &Chatlog) -> FullModel<LogId, ChatModel> {
@@ -40,4 +40,39 @@ pub fn channel_model_from_channel_list_data(
             push_alert: data.push_alert,
         },
     )
+}
+
+// TODO:: use trait
+pub fn channel_user_model_from_user_variant(
+    channel_id: ChannelId,
+    watermark: LogId,
+    user: &UserVariant,
+) -> FullModel<ChannelUserId, ChannelUserModel> {
+    match *user {
+        UserVariant::Normal(ref user) => FullModel::new(
+            user.user_id,
+            ChannelUserModel {
+                channel_id,
+                nickname: user.nickname.clone(),
+                profile_url: user.profile_image_url.clone(),
+                full_profile_url: user.full_profile_image_url.clone(),
+                original_profile_url: user.original_profile_image_url.clone(),
+                user_type: user.user_type,
+                watermark,
+            },
+        ),
+
+        UserVariant::Open(ref user) => FullModel::new(
+            user.user_id,
+            ChannelUserModel {
+                channel_id,
+                nickname: user.nickname.clone(),
+                profile_url: user.profile_image_url.clone(),
+                full_profile_url: user.full_profile_image_url.clone(),
+                original_profile_url: user.original_profile_image_url.clone(),
+                user_type: user.user_type,
+                watermark,
+            },
+        ),
+    }
 }
