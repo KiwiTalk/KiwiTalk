0f71ff26fb95d12015b20a6f7a4ca49c428a5d4b
storycraft <storycraft@pancake.sh>
2023-01-24 13:45:38 +0900

    feat(talk-client): add DisplayUser

--- crates/kiwi-talk-client/src/user/mod.rs
+++ crates/kiwi-talk-client/src/user/mod.rs
@@ -1,1 +1,12 @@
+use serde::{Deserialize, Serialize};
 
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct DisplayUser {
+    pub user_id: i64,
+
+    pub nickname: String,
+
+    pub profile_image_url: Option<String>,
+
+    pub country_iso: Option<String>,
+}
