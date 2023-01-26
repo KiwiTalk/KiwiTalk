cdd7a20efd4072d6e271c2c9066db78b4ffbc445
storycraft <storycraft@pancake.sh>
2023-01-26 14:54:24 +0900

    feat: remove old conversion

--- crates/kiwi-talk-client/src/database/conversion.rs
+++ crates/kiwi-talk-client/src/database/conversion.rs
@@ -7,26 +7,6 @@ use super::{channel::model::ChannelUserModel, chat::model::ChatModel, model::Ful
 use talk_loco_command::structs::{chat::Chatlog, user::UserVariant};
 
 // TODO:: use trait
-pub fn chat_model_from_chatlog(chatlog: &Chatlog) -> FullModel<LogId, ChatModel> {
-    FullModel::new(
-        chatlog.log_id,
-        ChatModel {
-            channel_id: chatlog.chat_id,
-            prev_log_id: chatlog.prev_log_id,
-            chat_type: chatlog.chat_type,
-            message_id: chatlog.msg_id,
-            send_at: chatlog.send_at,
-            author_id: chatlog.author_id,
-            message: chatlog.message.clone(),
-            attachment: chatlog.attachment.clone(),
-            supplement: chatlog.supplement.clone(),
-            referer: chatlog.referer,
-            deleted: false,
-        },
-    )
-}
-
-// TODO:: use trait
 pub fn channel_user_model_from_user_variant(
     channel_id: ChannelId,
     watermark: LogId,
