f4d3c883a25526d4d59ee22042c3c094b2ca3ec4
storycraft <storycraft@pancake.sh>
2023-02-01 02:10:00 +0900

    feat: implement sidebar component

---
+++ frontend/src/app/components/sidebar/sidebar.stories.tsx
@@ -0,0 +1,26 @@
+import { StoryFn } from '@storybook/react';
+import styled from 'styled-components';
+import { Sidebar, SidebarProp } from '.';
+
+export default {
+  title: 'KiwiTalk/app/Sidebar',
+  component: Sidebar,
+  argTypes: {
+    onMenuSelect: { action: 'MenuItem' },
+    onButtonClick: { action: 'ButtonItem' },
+  },
+};
+
+const PreviewSidebar = styled(Sidebar)`
+  border: solid black 1px;
+
+  height: 720px;
+`;
+
+const Template: StoryFn<SidebarProp> = (args) =>
+  <PreviewSidebar {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+
+};
