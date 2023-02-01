7d7f014841172e7226c4d345135babd95921f030
storycraft <storycraft@pancake.sh>
2023-02-01 14:09:50 +0900

    feat: change render root

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,14 +4,9 @@ import { App } from './app';
 import './global.css';
 
 function main() {
-  const rootElement = document.getElementById('root');
-  if (!rootElement) {
-    throw new Error('Cannot find root element');
-  }
-
-  const root = createRoot(rootElement);
+  const root = createRoot(document.body);
 
   root.render(<React.StrictMode><App /></React.StrictMode>);
 }
 
-window.addEventListener('DOMContentLoaded', () => main());
+main();
