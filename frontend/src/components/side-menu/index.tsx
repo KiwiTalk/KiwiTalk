44d08cb22e2fdfab055c707ec8985b88920918e8
storycraft <storycraft@pancake.sh>
2023-02-02 13:37:28 +0900

    feat: implement side menu

---
+++ frontend/src/components/side-menu/index.tsx
@@ -0,0 +1,66 @@
+import { Children, PropsWithChildren, ReactNode } from 'react';
+import styled from 'styled-components';
+
+export type SideMenuProp = {
+  name: string,
+  headContents?: ReactNode,
+  className?: string,
+};
+
+export const SideMenu = ({
+  name,
+  headContents,
+  className,
+
+  children,
+}: PropsWithChildren<SideMenuProp>) => {
+  return <Container className={className}>
+    <Head>
+      <Name>{name}</Name>
+      {headContents ? <HeadContainer>{headContents}</HeadContainer> : null}
+    </Head>
+    {
+      Children.count(children) > 0 ?
+        <ContentList>
+          {Children.map(children, (value, idx) => {
+            return <ContentItem key={idx}>{value}</ContentItem>;
+          })}
+        </ContentList> : null
+    }
+  </Container>;
+};
+
+const Container = styled.div`
+  margin: 1rem 1rem 0px 1rem;
+`;
+
+const Head = styled.div`
+  display: flex;
+
+  align-items: center;
+`;
+
+const Name = styled.span`
+  font-size: 1.5rem;
+
+  user-select: None;
+
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+
+const HeadContainer = styled.div`
+  margin-left: auto;
+`;
+
+const ContentList = styled.ul`
+  margin: 0px;
+  padding: 0px;
+
+  list-style-type: none;
+`;
+
+const ContentItem = styled.li`
+  margin-top: 0.5rem;
+  padding 0px;
+`;
---
+++ frontend/src/components/side-menu/side-menu.stories.tsx
@@ -0,0 +1,30 @@
+import { StoryFn } from '@storybook/react';
+import styled from 'styled-components';
+import { SideMenu, type SideMenuProp } from '.';
+
+export default {
+  title: 'KiwiTalk/SideMenu',
+  component: SideMenu,
+};
+
+const Container = styled.div`
+  border: 1px solid #000000;
+  width: 640px;
+  height: 480px;
+`;
+
+const Template: StoryFn<SideMenuProp> = (args) => {
+  return <Container>
+    <SideMenu {...args}>
+      <div>A</div>
+      <div>B</div>
+      <div>C</div>
+    </SideMenu>
+  </Container>;
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  name: 'Example',
+  headContents: 'Head contents',
+};
