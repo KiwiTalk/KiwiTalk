69c64433de5e08c8ba0669762bb3c3eae496c36b
storycraft <storycraft@pancake.sh>
2023-09-17 09:45:09 +0900

    feat: remove dbg

--- crates/talk-loco-client/examples/booking.rs
+++ crates/talk-loco-client/examples/booking.rs
@@ -1,5 +1,6 @@
-use std::error::Error;
+use std::{error::Error, pin::pin};
 
+use futures_lite::StreamExt;
 use talk_loco_client::{client::booking::BookingClient, session::LocoSession, LocoClient};
 use talk_loco_command::request;
 use tokio::{io::BufStream, net::TcpStream};
@@ -23,7 +24,13 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .unwrap()
         .compat();
 
-    let (session, _) = LocoSession::new(LocoClient::new(stream));
+    let (session, stream) = LocoSession::new(LocoClient::new(stream));
+
+    tokio::spawn(async move {
+        let mut stream = pin!(stream);
+
+        while let Some(_) = stream.next().await {}
+    });
 
     let booking_client = BookingClient(&session);
 
