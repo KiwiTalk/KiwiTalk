3a48dbc3b12dd9947509d61a28cc2b7b0c77702d
storycraft <storycraft@pancake.sh>
2023-09-24 13:27:03 +0900

    feat: move channel_id to ClientEvent::Channel

--- crates/kiwi-talk-client/src/event/channel.rs
+++ crates/kiwi-talk-client/src/event/channel.rs
@@ -1,7 +1,7 @@
 use serde::{Deserialize, Serialize};
 
 use crate::{
-    channel::{user::UserId, ChannelId},
+    channel::user::UserId,
     chat::{Chatlog, LogId},
 };
 
@@ -14,7 +14,6 @@ pub enum ChannelEvent {
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct ChatReceived {
-    pub channel_id: ChannelId,
     pub link_id: Option<i64>,
 
     pub log_id: LogId,
@@ -24,9 +23,6 @@ pub struct ChatReceived {
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct ChatRead {
-    /// Channel id
-    pub channel_id: ChannelId,
-
     /// Read user id
     pub user_id: UserId,
 
--- crates/kiwi-talk-client/src/event/mod.rs
+++ crates/kiwi-talk-client/src/event/mod.rs
@@ -1,11 +1,8 @@
 pub mod channel;
 
-use std::error::Error;
+use serde::Serialize;
 
-use serde::{Deserialize, Serialize, Serializer};
-use talk_loco_client::command::Command;
-
-use crate::handler::error::HandlerError;
+use crate::channel::ChannelId;
 
 use self::channel::ChannelEvent;
 
@@ -13,50 +10,11 @@ use self::channel::ChannelEvent;
 #[serde(tag = "type", content = "data")]
 pub enum ClientEvent {
     /// Channel event
-    Channel(ChannelEvent),
+    Channel { id: ChannelId, event: ChannelEvent },
 
     /// Server switch request
     SwitchServer,
 
     /// Kickout reason
     Kickout(i16),
-
-    Unhandled(EventCommand),
-
-    #[serde(serialize_with = "serialize_error_to_string")]
-    Error(HandlerError),
-}
-
-impl From<HandlerError> for ClientEvent {
-    fn from(err: HandlerError) -> Self {
-        Self::Error(err)
-    }
-}
-
-impl From<ChannelEvent> for ClientEvent {
-    fn from(channel_event: ChannelEvent) -> Self {
-        Self::Channel(channel_event)
-    }
-}
-
-fn serialize_error_to_string<E: Error, S>(error: E, serializer: S) -> Result<S::Ok, S::Error>
-where
-    S: Serializer,
-{
-    serializer.serialize_str(&error.to_string())
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct EventCommand {
-    pub method: String,
-    pub data_type: u8,
-}
-
-impl<T> From<Command<T>> for EventCommand {
-    fn from(command: Command<T>) -> Self {
-        Self {
-            method: command.header.method.to_string(),
-            data_type: command.header.data_type,
-        }
-    }
 }
