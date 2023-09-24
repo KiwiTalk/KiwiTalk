61aad8821689225d48eca971340b7b17d99a1119
storycraft <storycraft@pancake.sh>
2023-09-24 13:28:00 +0900

    feat: new handler

---
+++ crates/kiwi-talk-client/src/handler/command.rs
@@ -0,0 +1,64 @@
+use serde::Deserialize;
+
+use crate::chat::Chatlog;
+
+/// Send before server disconnect connection
+#[derive(Debug, Clone, Deserialize)]
+pub struct Kickout {
+    /// Kicked reasoon
+    ///
+    /// * Change server = 2
+    /// * Login another = 0
+    /// * Account deleted = 1
+    pub reason: i16,
+}
+
+/// Message sent from chatroom
+#[derive(Debug, Clone, Deserialize)]
+pub struct Msg {
+    /// Sent chatroom id
+    #[serde(rename = "chatId")]
+    pub chat_id: i64,
+
+    /// Sent chat log id
+    #[serde(rename = "logId")]
+    pub log_id: i64,
+
+    #[serde(rename = "chatLog")]
+    pub chatlog: Chatlog,
+
+    /// Sender nickname
+    #[serde(rename = "authorNickname")]
+    pub author_nickname: Option<String>,
+
+    /// false If sender sent message without reading.
+    ///
+    /// If it's false, sent message doesn't decrease read count of last chat.
+    #[serde(rename = "noSeen")]
+    pub no_seen: bool,
+
+    #[serde(rename = "li")]
+    pub link_id: Option<i64>,
+
+    /// Act like no_seen.(?)
+    /// Only appears on openchat
+    #[serde(rename = "notiRead")]
+    pub noti_read: Option<bool>,
+}
+
+/// Message read by someone
+#[derive(Debug, Clone, Deserialize)]
+pub struct DecunRead {
+    /// Chatroom id
+    #[serde(rename = "chatId")]
+    pub chat_id: i64,
+
+    /// Read user id
+    #[serde(rename = "userId")]
+    pub user_id: i64,
+
+    /// Read message log id
+    ///
+    /// Official client decrease every unread chat read count till this chat.
+    pub watermark: i64,
+}
--- crates/kiwi-talk-client/src/handler/mod.rs
+++ crates/kiwi-talk-client/src/handler/mod.rs
@@ -1,1 +1,93 @@
+mod command;
 pub mod error;
+
+use talk_loco_client::command::Command;
+
+use crate::{
+    channel::user::UserId,
+    database::pool::DatabasePool,
+    event::{
+        channel::{ChannelEvent, ChatRead, ChatReceived},
+        ClientEvent,
+    },
+    KiwiTalkClient,
+};
+
+use self::{
+    command::{DecunRead, Kickout, Msg},
+    error::HandlerError,
+};
+
+type HandlerResult = Result<Option<ClientEvent>, HandlerError>;
+
+#[derive(Debug, Clone)]
+pub struct ClientHandler {
+    user_id: UserId,
+    pool: DatabasePool,
+}
+
+impl ClientHandler {
+    pub fn new(client: &KiwiTalkClient) -> Self {
+        Self {
+            user_id: client.user_id(),
+            pool: client.pool().clone(),
+        }
+    }
+
+    pub async fn handle(&self, command: &Command<impl AsRef<[u8]>>) -> HandlerResult {
+        macro_rules! create_handler {
+            (
+                $($method:literal => $handler:path),* $(,)?
+            ) => {
+                match &*command.header.method {
+                    $(
+                        $method => $handler(self, ::bson::from_slice(command.data.as_ref())?)?,
+                    )*
+
+                    _ => None,
+                }
+            };
+        }
+
+        Ok(create_handler!(
+            "KICKOUT" => on_kickout,
+            "CHANGESVR" => on_switch_server,
+
+            "MSG" => on_chat,
+            "DECUNREAD" => on_chat_read,
+        ))
+    }
+}
+
+fn on_kickout(_: &ClientHandler, kickout: Kickout) -> HandlerResult {
+    Ok(Some(ClientEvent::Kickout(kickout.reason)))
+}
+
+fn on_switch_server(_: &ClientHandler, _: ()) -> HandlerResult {
+    Ok(Some(ClientEvent::SwitchServer))
+}
+
+fn on_chat(_handler: &ClientHandler, msg: Msg) -> HandlerResult {
+    Ok(Some(ClientEvent::Channel {
+        id: msg.chat_id,
+
+        event: ChannelEvent::Chat(ChatReceived {
+            link_id: msg.link_id,
+
+            log_id: msg.log_id,
+            user_nickname: msg.author_nickname,
+            chat: msg.chatlog,
+        }),
+    }))
+}
+
+fn on_chat_read(_handler: &ClientHandler, read: DecunRead) -> HandlerResult {
+    Ok(Some(ClientEvent::Channel {
+        id: read.chat_id,
+
+        event: ChannelEvent::ChatRead(ChatRead {
+            user_id: read.user_id,
+            log_id: read.watermark,
+        }),
+    }))
+}
