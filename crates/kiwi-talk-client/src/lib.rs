d164c1ed1e71365fede070ef16cec5dd88c48f6d
storycraft <storycraft@pancake.sh>
2023-09-24 13:26:33 +0900

    feat: move status and login

--- crates/kiwi-talk-client/src/lib.rs
+++ crates/kiwi-talk-client/src/lib.rs
@@ -5,7 +5,6 @@ pub mod database;
 pub mod error;
 pub mod event;
 pub mod handler;
-pub mod status;
 
 use std::{io, pin::pin};
 
@@ -13,15 +12,14 @@ use channel::{normal::ClientNormalChannel, user::UserId, ChannelId, ClientChanne
 use config::ClientConfig;
 use database::pool::DatabasePool;
 use error::ClientError;
-use futures::{AsyncRead, AsyncWrite, StreamExt, TryStreamExt};
-use status::ClientStatus;
+use futures::{AsyncRead, AsyncWrite, TryStreamExt};
+use serde::{Deserialize, Serialize};
 use talk_loco_client::{
     session::LocoSession,
     talk::{LChatListReq, LoginListReq, SetStReq, TalkSession},
-    LocoClient, RequestError,
+    RequestError,
 };
 use thiserror::Error;
-use tokio::select;
 
 #[derive(Debug, Clone)]
 pub struct KiwiTalkClient {
@@ -62,74 +60,50 @@ impl KiwiTalkClient {
     }
 
     pub async fn login<S>(
-        stream: S,
+        session: LocoSession,
         pool: DatabasePool,
-        info: ClientConfig<'_>,
+        config: ClientConfig<'_>,
         credential: ClientCredential<'_>,
         status: ClientStatus,
     ) -> Result<Self, LoginError>
     where
         S: AsyncRead + AsyncWrite + Unpin,
     {
-        let (session, mut stream) = LocoSession::new(LocoClient::new(stream));
+        let mut login_res = TalkSession(&session)
+            .login(&LoginListReq {
+                os: config.os,
+                net_type: config.net_type,
+                app_version: config.app_version,
+                mccmnc: config.mccmnc,
+                protocol_version: "1.0",
+                device_uuid: credential.device_uuid,
+                oauth_token: credential.access_token,
+                language: config.language,
+                device_type: Some(config.device_type),
+                pc_status: Some(status as _),
+                revision: None,
+                rp: [0x00, 0x00, 0xff, 0xff, 0x00, 0x00],
+                chat_list: LChatListReq {
+                    chat_ids: &[],
+                    max_ids: &[],
+                    last_token_id: 0,
+                    last_chat_id: None,
+                },
+                last_block_token: 0,
+                background: None,
+            })
+            .await?;
 
-        let mut stream_buffer = Vec::new();
-        let login_res = {
-            let login_task = pin!(async {
-                let mut login_res = TalkSession(&session)
-                    .login(&LoginListReq {
-                        os: info.os,
-                        net_type: info.net_type,
-                        app_version: info.app_version,
-                        mccmnc: info.mccmnc,
-                        protocol_version: "1.0",
-                        device_uuid: credential.device_uuid,
-                        oauth_token: credential.access_token,
-                        language: info.language,
-                        device_type: Some(info.device_type),
-                        pc_status: Some(status as _),
-                        revision: None,
-                        rp: [0x00, 0x00, 0xff, 0xff, 0x00, 0x00],
-                        chat_list: LChatListReq {
-                            chat_ids: &[],
-                            max_ids: &[],
-                            last_token_id: 0,
-                            last_chat_id: None,
-                        },
-                        last_block_token: 0,
-                        background: None,
-                    })
-                    .await?;
+        if !login_res.chat_list.eof {
+            let mut stream = pin!(TalkSession(&session).channel_list_stream(0, None));
 
-                if !login_res.chat_list.eof {
-                    let mut stream = pin!(TalkSession(&session).channel_list_stream(0, None));
-
-                    while let Some(mut res) = stream.try_next().await? {
-                        login_res.chat_list.chat_datas.append(&mut res.chat_datas);
-                    }
-                }
-
-                Ok::<_, LoginError>(login_res)
-            });
-
-            let stream_task = pin!(async {
-                while let Some(next) = stream.next().await {
-                    let next = match next {
-                        Ok(next) => next,
-                        Err(err) => return LoginError::Io(err),
-                    };
-
-                    stream_buffer.push(next);
-                }
-
-                LoginError::SessionClosed
-            });
-
-            select! {
-                res = login_task => res?,
-                err = stream_task => return Err(err),
+            while let Some(mut res) = stream.try_next().await? {
+                login_res.chat_list.chat_datas.append(&mut res.chat_datas);
             }
-        };
+        }
+
+        // TODO:: implement channel update
+        let _update_list = login_res.chat_list.chat_datas;
 
         Ok(Self {
             user_id: login_res.user_id,
@@ -156,6 +130,14 @@ pub struct ClientCredential<'a> {
     pub access_token: &'a str,
     pub device_uuid: &'a str,
     pub user_id: Option<i64>,
+}
+
+#[repr(i32)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize)]
+#[serde(tag = "status")]
+pub enum ClientStatus {
+    Unlocked = 1,
+    Locked = 2,
 }
 
 pub type ClientResult<T> = Result<T, ClientError>;
--- crates/kiwi-talk-client/src/status.rs
+++
@@ -1,9 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-#[repr(i32)]
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize)]
-#[serde(tag = "status")]
-pub enum ClientStatus {
-    Unlocked = 1,
-    Locked = 2,
-}
