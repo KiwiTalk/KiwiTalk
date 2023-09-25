bc49e0867a0a373a7c7abbf84d2f16cab1df32f0
storycraft <storycraft@pancake.sh>
2023-09-25 23:36:02 +0900

    feat: save window state on close

--- frontend/src/app/window/control.tsx
+++ frontend/src/app/window/control.tsx
@@ -1,5 +1,6 @@
 import { appWindow } from '@tauri-apps/api/window';
 import { ControlButtons, ControlType, WindowControl } from '../../components/window/control';
+import { saveWindowState, StateFlags } from 'tauri-plugin-window-state-api';
 
 export type AppWindowControlProp = {
   buttons?: ControlButtons;
@@ -10,7 +11,7 @@ export const AppWindowControl = (props: AppWindowControlProp) => {
   function onControlClick(type: ControlType) {
     switch (type) {
       case 'close': {
-        appWindow.close().then();
+        saveWindowState(StateFlags.ALL).then(() => appWindow.close()).then();
         break;
       }
 
