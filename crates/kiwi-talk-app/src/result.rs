40d7621a0838882461ce1887f4981b0e2823f06a
storycraft <storycraft@pancake.sh>
2023-09-27 12:27:02 +0900

    feat: use deref

--- crates/kiwi-talk-app/src/result.rs
+++ crates/kiwi-talk-app/src/result.rs
@@ -1,14 +1,23 @@
 use serde::Serialize;
-use std::fmt::Write;
+use std::{fmt::Write, ops::Deref};
 
 pub type TauriResult<T> = Result<T, TauriAnyhowError>;
 
+#[derive(Debug)]
 #[repr(transparent)]
 pub struct TauriAnyhowError(anyhow::Error);
 
 impl<T: Into<anyhow::Error>> From<T> for TauriAnyhowError {
     fn from(err: T) -> Self {
         Self(err.into())
+    }
+}
+
+impl Deref for TauriAnyhowError {
+    type Target = anyhow::Error;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
     }
 }
 
