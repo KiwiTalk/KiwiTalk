ba184328a5f1d74563e5d146d0128e2838e564f6
storycraft <storycraft@pancake.sh>
2023-09-29 12:42:58 +0900

    feat: use transaction on task

--- crates/kiwi-talk-client/src/database/channel/updater.rs
+++ crates/kiwi-talk-client/src/database/channel/updater.rs
@@ -1,4 +1,3 @@
-use nohash_hasher::IntMap;
 use rusqlite::Connection;
 use talk_loco_client::talk::session::load_channel_list::response::ChannelListData as LocoChanneListData;
 
@@ -17,27 +16,18 @@ pub impl Connection {
 pub struct ChannelUpdater<'a>(pub &'a Connection);
 
 impl ChannelUpdater<'_> {
-    pub fn update(
-        self,
-        iter: impl IntoIterator<Item = LocoChanneListData>,
-    ) -> Result<(), rusqlite::Error> {
-        let transaction = self.0.transaction()?;
-
-        for list_data in iter {
-            transaction.channel().insert_or_replace(&ChannelUpdateRow {
-                id: list_data.id,
-                channel_type: list_data.channel_type,
-                last_seen_log_id: list_data.last_seen_log_id,
-                last_update: list_data.last_update,
-            })?;
-        }
-
-        transaction.commit()?;
+    pub fn update(self, list_data: LocoChanneListData) -> Result<(), rusqlite::Error> {
+        self.0.channel().insert_or_replace(&ChannelUpdateRow {
+            id: list_data.id,
+            channel_type: list_data.channel_type,
+            last_seen_log_id: list_data.last_seen_log_id,
+            last_update: list_data.last_update,
+        })?;
 
         Ok(())
     }
 
-    pub fn get_update_map(self) -> Result<IntMap<ChannelId, i64>, rusqlite::Error> {
+    pub fn get_update_map<B: FromIterator<(ChannelId, i64)>>(self) -> Result<B, rusqlite::Error> {
         let mut statement = self.0.prepare("SELECT id, last_update FROM channel")?;
 
         let rows = statement.query(())?;
