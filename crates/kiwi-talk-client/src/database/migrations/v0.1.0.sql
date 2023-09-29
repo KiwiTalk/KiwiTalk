b32feec1d346d3862fd4ac075654140d00c94f07
storycraft <storycraft@pancake.sh>
2023-09-29 12:29:57 +0900

    feat: use id and type as primary key

--- crates/kiwi-talk-client/src/database/migrations/v0.1.0.sql
+++ crates/kiwi-talk-client/src/database/migrations/v0.1.0.sql
@@ -16,11 +16,13 @@ CREATE TABLE IF NOT EXISTS chat (
 );
 
 CREATE TABLE IF NOT EXISTS channel_update (
-    id INTEGER PRIMARY KEY NOT NULL,
+    id INTEGER NOT NULL,
     type VARCHAR(16) NOT NULL,
 
     last_seen_log_id INTEGER NOT NULL,
-    last_update INTEGER NOT NULL
+    last_update INTEGER NOT NULL,
+
+    PRIMARY KEY(id, type)
 );
 
 CREATE TABLE IF NOT EXISTS channel_meta (
