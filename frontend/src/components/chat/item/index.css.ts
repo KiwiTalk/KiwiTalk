e6cfebbf030cccd8de47e5943068d281c4af0324
Su-Yong <simssy2205@gmail.com>
2023-10-05 17:44:33 +0900

    fix(chat-item): fix chat image size bug

--- frontend/src/components/chat/item/index.css.ts
+++ frontend/src/components/chat/item/index.css.ts
@@ -19,7 +19,6 @@ export const container = style({
   display: 'grid',
   gridTemplateColumns: '50px 1fr auto',
   gridTemplateRows: 'auto auto',
-  alignContent: 'center',
 
   width: '100%',
   height: 50,
