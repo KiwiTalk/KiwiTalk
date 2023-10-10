26b5dc5a7c3258437eff5541cf539030561d2036
storycraft <storycraft@pancake.sh>
2023-10-10 19:10:52 +0900

    feat: add FriendItem component

---
+++ frontend/src/components/friend-item/index.css.ts
@@ -0,0 +1,38 @@
+import { style } from '@vanilla-extract/css';
+
+export const container = style({
+  display: 'flex',
+  flexDirection: 'row',
+  gap: 4,
+});
+
+export const profileImageBox = style({
+  margin: 8,
+  borderRadius: '50%',
+  overflow: 'hidden',
+  background: '#dddddd',
+  width: '2.25rem',
+  height: '2.25rem',
+});
+
+export const profileNameBox = style({
+  display: 'flex',
+  flexDirection: 'column',
+  margin: 'auto 0px',
+});
+
+export const profileImage = style({
+  width: '100%',
+  height: '100%',
+
+  objectFit: 'cover',
+});
+
+export const nicknameText = style({
+  fontSize: '1rem',
+});
+
+export const statusMessageText = style({
+  color: '#4D5061',
+  fontSize: '0.75rem',
+});
