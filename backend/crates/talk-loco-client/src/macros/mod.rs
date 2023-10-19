c8bc9f6c16429c3aeb7a4a239726989c91acf52d
storycraft <storycraft@pancake.sh>
2023-10-19 23:18:01 +0900

    feat: add request macro

--- backend/crates/talk-loco-client/src/macros/mod.rs
+++ backend/crates/talk-loco-client/src/macros/mod.rs
@@ -2,6 +2,52 @@
 pub mod __private;
 
 #[macro_export]
+macro_rules! request {
+    (
+        $session:expr, $method:literal, { $($body:tt)* } $($tt:tt)*
+    ) => {
+        async {
+            use $crate::macros::__private::{bson, loco_protocol};
+
+            let data = $session.request(
+                loco_protocol::command::Method::new($method).unwrap(),
+                bson::rawdoc!{
+                    $($body)*
+                }.into_bytes(),
+            )
+            .await.map_err(|_| $crate::RequestError::Write(::std::io::ErrorKind::UnexpectedEof.into()))?
+            .await.map_err(|_| $crate::RequestError::Read(::std::io::ErrorKind::UnexpectedEof.into()))?
+            .data;
+
+            request!(@inner(data = data) $($tt)*)
+        }
+    };
+
+    (
+        @inner(data = $data:ident)
+    ) => {
+        match bson::from_slice::<$crate::BsonCommandStatus>(&$data)?.status {
+            0 => Ok(()),
+            status => Err::<_, $crate::RequestError>($crate::RequestError::Status(status)),
+        }
+    };
+
+    (
+        @inner(data = $data:ident)
+        => {
+            $($status:pat => $ty:ty),* $(,)?
+        }
+    ) => {
+        
+        match bson::from_slice::<$crate::BsonCommandStatus>(&$data)?.status {
+            $($status => Ok(bson::from_slice::<$ty>(&$data)?),)*
+
+            status => Err::<_, $crate::RequestError>($crate::RequestError::Status(status)),
+        }
+    }
+}
+
+#[macro_export]
 macro_rules! impl_session {
     () => {
         compile_error!("Example usage: impl_session!(
