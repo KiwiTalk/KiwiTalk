3d2f3c43a16ce0e7c2e481712f5b6f90bd1d747b
storycraft <storycraft@pancake.sh>
2023-10-25 15:00:29 +0900

    fix: error

--- frontend/src/pages/main/channel/_components/channel-list/channel-list.stories.tsx
+++ frontend/src/pages/main/channel/_components/channel-list/channel-list.stories.tsx
@@ -6,6 +6,7 @@ import { ChannelList, ChannelListProps, ChannelListViewModelType } from './chann
 import IconSearch from '@/assets/icons/search.svg';
 import IconAddChat from '@/pages/main/channel/_assets/icons/add-chat.svg';
 import { createSignal } from 'solid-js';
+import { ListProfile } from '../../_types/channel-list-item';
 
 export default {
   title: 'KiwiTalk v2/Channel/Channel List',
@@ -14,10 +15,10 @@ export default {
 
 const Template: StoryFn<ChannelListProps> = () => {
   const getUsers = (length: number) => Array.from({ length })
-    .map((_, index) => ({
+    .map((_, index) => [`${index}`, {
       nickname: `User ${index + 1}`,
       profileUrl: `https://picsum.photos/200?s=${Math.random()}`,
-    }));
+    }] as [string, ListProfile]);
 
   const NoOpViewModel: ChannelListViewModelType = () => ({
     channels: () => Array.from({ length: 10 }).map((_, index) => {
@@ -26,11 +27,9 @@ const Template: StoryFn<ChannelListProps> = () => {
 
       const lastChat = {
         chatType: 0,
-        nickname: displayUsers[Math.floor(Math.random() * userLength)].nickname,
-        content: {
-          message: 'last message',
-          timestamp: new Date(),
-        },
+        nickname: displayUsers[Math.floor(Math.random() * userLength)][1].nickname,
+        content: 'last message',
+        timestamp: new Date(),
       };
 
       return {
