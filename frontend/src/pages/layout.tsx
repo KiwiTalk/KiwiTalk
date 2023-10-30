cd855341e2f733aef6f02e4843b823c3ba3b1c18
Su-Yong <simssy2205@gmail.com>
2023-10-30 11:18:57 +0900

    refactor(frontend): refactor `WindowControls` viewModel

--- frontend/src/pages/layout.tsx
+++ frontend/src/pages/layout.tsx
@@ -1,13 +1,16 @@
-import { ParentProps } from 'solid-js';
+import { ParentProps, createResource, onCleanup } from 'solid-js';
+import { Router, Routes, hashIntegration } from '@solidjs/router';
+import { Transition } from 'solid-transition-group';
+import { appWindow } from '@tauri-apps/api/window';
 
 import { I18nProvider } from '@/features/i18n';
 import { ConfigProvider } from '@/features/config';
+import { classes, themeRoot } from '@/features/theme';
 
 import { WindowControls } from './_components/window-controls';
-import { Router, Routes, hashIntegration } from '@solidjs/router';
-import { classes, themeRoot } from '@/features/theme';
-import { container } from './layout.css';
-import { Transition } from 'solid-transition-group';
+
+import * as styles from './layout.css';
+import { saveWindowState, StateFlags } from 'tauri-plugin-window-state-api';
 
 export const Provider = (props: ParentProps) => (
   <I18nProvider>
@@ -17,22 +20,46 @@ export const Provider = (props: ParentProps) => (
   </I18nProvider>
 );
 
-export const Layout = (props: ParentProps) => (
-  <Provider>
-    <Router source={hashIntegration()}>
-      <div
-        classList={{
-          [themeRoot]: true,
-          [container]: true,
-        }}
-      >
-        <WindowControls />
-        <Transition appear mode={'outin'} {...classes.transition.scale}>
-          <Routes>
-            {props.children}
-          </Routes>
-        </Transition>
-      </div>
-    </Router>
-  </Provider>
-);
+export const Layout = (props: ParentProps) => {
+  const [isFocused, { mutate: mutateIsFocused }] = createResource(
+    () => appWindow.isFocused(),
+  );
+
+  const [unlisten] = createResource(
+    () => appWindow.onFocusChanged(({ payload: focused }) => mutateIsFocused(focused)),
+  );
+
+  const onMinimize = () => appWindow.minimize();
+  const onMaximize = () => appWindow.toggleMaximize();
+  const onClose = async () => {
+    await saveWindowState(StateFlags.ALL);
+    await appWindow.close();
+  };
+
+  onCleanup(() => unlisten()?.());
+
+  return (
+    <Provider>
+      <Router source={hashIntegration()}>
+        <div
+          classList={{
+            [themeRoot]: true,
+            [styles.container]: true,
+          }}
+        >
+          <WindowControls
+            isActive={isFocused()}
+            onMinimize={onMinimize}
+            onMaximize={onMaximize}
+            onClose={onClose}
+          />
+          <Transition appear mode={'outin'} {...classes.transition.scale}>
+            <Routes>
+              {props.children}
+            </Routes>
+          </Transition>
+        </div>
+      </Router>
+    </Provider>
+  );
+};
