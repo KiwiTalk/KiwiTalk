b2eccd533cc1e069e95bd04dd48737e2951a9340
Su-Yong <simssy2205@gmail.com>
2023-11-06 18:18:55 +0900

    feat(frontend): add `Loader`

---
+++ frontend/src/ui-common/loader/loader.tsx
@@ -0,0 +1,43 @@
+import { assignInlineVars } from '@vanilla-extract/dynamic';
+import * as styles from './loader.css';
+import { mergeProps } from 'solid-js';
+
+export type LoaderProps = {
+  duration?: string;
+  size?: string;
+}
+export const Loader = (props: LoaderProps) => {
+  const merged = mergeProps({
+    duration: '0.6s',
+    size: '8px',
+  }, props);
+
+  return (
+    <div
+      class={styles.container}
+      style={assignInlineVars({
+        [styles.duration]: merged.duration,
+        [styles.size]: merged.size,
+      })}
+    >
+      <div
+        class={styles.dot}
+        style={assignInlineVars({
+          [styles.delayIndex]: '0',
+        })}
+      />
+      <div
+        class={styles.dot}
+        style={assignInlineVars({
+          [styles.delayIndex]: '1',
+        })}
+      />
+      <div
+        class={styles.dot}
+        style={assignInlineVars({
+          [styles.delayIndex]: '2',
+        })}
+      />
+    </div>
+  );
+};
