a52ca04343f464a8d9a4b3f5656c1ee77afeebdb
storycraft <storycraft@pancake.sh>
2023-11-08 23:04:17 +0900

    feat: apply clippy, rustfmt

--- backend/bin/client/src/event.rs
+++ backend/bin/client/src/event.rs
@@ -5,16 +5,11 @@ use crate::channel::{ChannelMeta, Chatlog};
 #[derive(Debug, Serialize)]
 #[serde(tag = "type", content = "content")]
 pub(crate) enum ClientEvent {
-    Channel {
-        id: String,
-        event: ChannelEvent,
-    },
+    Channel { id: String, event: ChannelEvent },
 
     SwitchServer,
 
-    Kickout {
-        reason: i32,
-    },
+    Kickout { reason: i32 },
 }
 
 #[derive(Debug, Serialize)]
@@ -23,7 +18,10 @@ pub(crate) enum ChannelEvent {
     Chat(Chatlog),
 
     #[serde(rename_all = "camelCase")]
-    ChatRead { user_id: String, log_id: String },
+    ChatRead {
+        user_id: String,
+        log_id: String,
+    },
 
     ChatDeleted(Chatlog),
 
